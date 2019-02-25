73d4ced62afacbd987179b45f9d6e0005f757b47
Micah Schuster <micahdschuster@gmail.com>
2019-02-25 14:10:16 -0500

    Add transition between animations
    Add transition state betwen animations to go from run to idle.
    
    Add code to flip player when running the opposite direction that the sprite image is facing.
    
    This causes issues with the highlighing and will need to be adjusted to a new sprite depending on which way the player is facing.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,21 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class Player : MonoBehaviour
 {
     public float speed;   //available in the inspector
+    private float input;
 
     Rigidbody2D rb; //needs a rigidbody
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update() {
+        if (input != 0) {
+            anim.SetBool("isRunning", true);
+        } else {
+            anim.SetBool("isRunning", false);
+        }
+
+        //FIXME: Need to change sprite so that the highlighting on the sprite
+        //is correct. For now, this is ok.
+        if (input > 0) {
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+        else if(input < 0){
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
     }
 
     void FixedUpdate() {
         //geting player input
-        float input = Input.GetAxis("Horizontal"); //Has smoothing
+        input = Input.GetAxis("Horizontal"); //Has smoothing
         //float input = Input.GetAxisRaw("Horizontal"); //Has no smoothing
 
         //move player
