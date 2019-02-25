c7dfeb9656aa36764d7629ffffeeb285f6761ae1
Micah Schuster <micahdschuster@gmail.com>
2019-02-25 13:14:40 -0500

    Add movement script and box colliders
    Add player script that reads arrow or wasd keys for movement and uses the rigidbody velocity to move the player.
    
    Add wall box collider to restrict movement within the camera view and box collider to player.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    public float speed;   //available in the inspector
+
+    Rigidbody2D rb; //needs a rigidbody
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate() {
+        //geting player input
+        float input = Input.GetAxis("Horizontal"); //Has smoothing
+        //float input = Input.GetAxisRaw("Horizontal"); //Has no smoothing
+
+        //move player
+        rb.velocity = new Vector2(input*speed,rb.velocity.y);
+    }
+}
