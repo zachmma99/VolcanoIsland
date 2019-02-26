9a38153a2d8d77c89ad5cc4787fa33f739cc16e2
Micah Schuster <micahdschuster@gmail.com>
2019-02-26 10:03:51 -0500

    Add spawn behavior
    Adds 10 spawn points and a spawn script that randomly selects a spawn point and enemy to instantiate in the world.
    
    Also adjusted enemy to deal 2 damage.
    
    When player dies, spawning stops.

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    //inspector arrays for the spawn location objects and enemy prefabs
+    public Transform[] spawnLocations;
+    public GameObject[] enemies;
+
+    //public variables to control spawn behavior
+    public float timeBetweenSpawns;
+
+    private float spawnTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnTimer = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //stop spawning if there is no character
+        if (GameObject.FindGameObjectWithTag("Player") == null) {
+            return;
+        }
+        if (spawnTimer <= 0) {
+            //spawn a enemy
+            GameObject enemy = enemies[Random.Range(0, enemies.Length)];
+            Transform location = spawnLocations[Random.Range(0, spawnLocations.Length)].transform;
+            Instantiate(enemy,new Vector3(location.position.x,location.position.y,0f),Quaternion.identity);
+            spawnTimer = timeBetweenSpawns;
+        } else {
+            spawnTimer -= Time.deltaTime;
+        }
+
+    }
+}
