a977a013280e91f59acacb506700a0bf76f060f0
Micah Schuster <micahdschuster@gmail.com>
2019-02-26 08:23:37 -0500

    Add enemy sprites and enemy movement script
    Add three enemy sprites (two total enemies) and create the script to move them downward.
    
    Still need to animate one of the enemies.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    //two speeds to pick a random value between
+    public float minSpeed;
+    public float maxSpeed;
+
+    private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //pick random balue between min and max speed
+        speed = Random.Range(minSpeed, maxSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //translate this object down at speed
+        this.transform.Translate(Vector3.down * speed * Time.deltaTime);
+    }
+}
