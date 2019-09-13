1281fa077f9891813456474df90383d27f22450d
Micah Schuster <micahdschuster@gmail.com>
2019-09-13 12:23:53 -0400

    Add MainMenu Scene
    Add relevant scripts for when graphics and background music get added to the main menu.

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicManager : MonoBehaviour
+{
+
+
+    void Awake(){
+        //keeps this gameobject between scene transitions
+        //so that the background music will keep playing
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //play background music here
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
